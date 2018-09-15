1128ef50179fe584c1462a96c24f0aef9ab1ffcb
Kat Michaela <k@gothfem.me>
2018-09-15 15:48:02 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Kat."
+  end
+
+  get '/hometown' do
+    "My hometown is New Rochelle."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Run Away With Me by Carly Rae Jepsen."
+  end
 end
