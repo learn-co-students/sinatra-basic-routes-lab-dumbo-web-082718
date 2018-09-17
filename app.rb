80b6c14f54d509706a39a6a24bdab68f97f085b4
Malcome Fein <mfein90@gmail.com>
2018-09-17 10:34:31 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
+# It handles all the incoming requests from users
+# and directs the browser to the correct URL.
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+  "Hello, World!"
+end
+
+get '/name' do
+  "My name is Malcome"
+end
+
+get '/hometown' do
+  "My hometown is Manhattan"
+end
+
+get '/favorite-song' do
+  "My favorite song is uncanny valley"
+end
+
 end
