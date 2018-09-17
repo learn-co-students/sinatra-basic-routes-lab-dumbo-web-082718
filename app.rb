e71a754d694dfa653ec9785df820b6359fafa1e7
Andy <wang.andy32@gmail.com>
2018-09-16 22:27:44 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  get '/name' do
+    "My name is Andy."
+  end
+  get '/hometown' do
+    "My hometown is NYC."
+  end
+  get '/favorite-song' do
+    "My favorite song is Failure."
+  end
 end
