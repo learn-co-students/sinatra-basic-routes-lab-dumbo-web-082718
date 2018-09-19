e22d71de7589fde5af92de77a319c30aecd6bbdc
jordanmmartin <jdordan23@gmail.com>
2018-09-19 15:12:15 -0400

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Jordan."
+  end
+
+  get '/hometown' do
+    "My hometown is Kittaning, PA."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is When We Were Young by Adele."
+  end
+
 end
